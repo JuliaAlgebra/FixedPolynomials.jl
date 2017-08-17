d3db806ffa8dbf29ca91e75908e95c947a943ffa
Sascha Timme <sascha.timme@gmail.com>
2017-08-17 11:11:49 +0200

    Only use DynamicPolynomials for tests for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,10 @@ using FixedPolySystem
 using Base.Test
 const FPS = FixedPolySystem
 
-import TypedPolynomials
-Impl = TypedPolynomials
-include("poly_test.jl")
-include("system_test.jl")
+# import TypedPolynomials
+# Impl = TypedPolynomials
+# include("poly_test.jl")
+# include("system_test.jl")
 
 import DynamicPolynomials
 Impl = DynamicPolynomials
