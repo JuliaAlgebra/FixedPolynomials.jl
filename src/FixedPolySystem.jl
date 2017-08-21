3f32de8f93d4a1932c947630f4023b8fa2b6c941
Sascha Timme <sascha.timme@gmail.com>
2017-08-21 12:52:21 +0200

    Export AbstractPolySystem

--- src/FixedPolySystem.jl
+++ src/FixedPolySystem.jl
@@ -4,6 +4,7 @@ module FixedPolySystem
     import Base: start, next, done, length, eltype, show, ==, convert, promote_rule
 
     abstract type AbstractPolySystem{T} end
+    export AbstractPolySystem
 
     include("poly.jl")
     include("system.jl")
