184e26737d034bb88300c55f21feaccd60fd23f3
Sascha Timme <sascha.timme@gmail.com>
2017-08-21 12:44:20 +0200

    Createa and export AbstractPolySystem{T}

--- src/FixedPolySystem.jl
+++ src/FixedPolySystem.jl
@@ -3,6 +3,8 @@ module FixedPolySystem
     const MP = MultivariatePolynomials
     import Base: start, next, done, length, eltype, show, ==, convert, promote_rule
 
+    abstract type AbstractPolySystem{T} end
+
     include("poly.jl")
     include("system.jl")
     include("show.jl")
