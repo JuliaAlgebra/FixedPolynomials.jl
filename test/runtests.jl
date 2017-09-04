bfbf23ca66308120cf5fa865b79a3f8d6a5a692d
Sascha Timme <sascha.timme@gmail.com>
2017-09-04 16:59:45 +0200

    Clean up runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,26 +8,3 @@ using Base.Test
 import DynamicPolynomials
 Impl = DynamicPolynomials
 include("poly_test.jl")
-include("system_test.jl")
-
-
-Impl.@polyvar x y
-
-
-f = x + y
-import MultivariatePolynomials
-MP = MultivariatePolynomials
-Symbol.(MP.variables(f))
-
-Symbol(:x) == :x
-
-
-function test1(xs, x)
-    map(y -> y + x, xs)
-end
-
-function test2(xs, x)
-    map(+, xs, Base.Iterators.repeated(x))
-end
-
-import BenchmarkTools: @benchmark
