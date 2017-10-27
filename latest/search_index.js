ad5eaf08e3d7b3194bb0bb0963bd64ae85032e57
autodocs <autodocs>
2017-10-27 08:19:57 +0000

    build based on 8956ed8

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Tutorial",
     "category": "section",
-    "text": "Here is an example on how to create a Polynomial with Float64 coefficients:using FixedPolynomials\nimport DynamicPolynomials: @polyvar\n\n@polyvar x y z\n\nf = Polynomial{Float64}(x^2+y^3*z-2x*y)To evaluate f you simply have to pass in a Vector{Float64}x = rand(3)\nf(x) # alternatively evaluate(f, x)note: Note\nThe only defined method is evaluate(f::Polynomial{T}, x::AbstractVector{T}). This is intentional restrictive to avoid any unintended performance penalties.note: Note\nf has then the variable ordering as implied by DynamicPolynomials.variables(x^2+y^3*z-2x*y), i.e. f([1.0, 2.0, 3.0]) will evaluate f with x=1, y=2 and z=3."
+    "text": "Here is an example on how to create a Polynomial with Float64 coefficients:using FixedPolynomials\nimport DynamicPolynomials: @polyvar\n\n@polyvar x y z\n\nf = Polynomial{Float64}(x^2+y^3*z-2x*y)To evaluate f you simply have to pass in a Vector{Float64}x = rand(3)\nf(x) # alternatively evaluate(f, x)But this is note the fastest way possible. In order to achieve the best performance we need to precompute some things and also preallocate intermediate storage. For this we have GradientConfig and JacobianConfig. For single polynomial the API is as followscfg = GradientConfig(f) # this can be reused!\nf(x) == evaluate(f, x, cfg)\n# We can also compute the gradient of f at x\nmap(g -> g(x), ∇f) == gradient(f, x, cfg)We also have support for systems of polynomials:cfg = JacobianConfig([f, f]) # this can be reused!\n[f(x), f(x)] == evaluate([f, f] x, cfg)\n# We can also compute the jacobian of [f, f] at x\njacobian(f, x, cfg)Make sure to also check out GradientDiffResult and JacobianDiffResult.note: Note\nf has then the variable ordering as implied by DynamicPolynomials.variables(x^2+y^3*z-2x*y), i.e. f([1.0, 2.0, 3.0]) will evaluate f with x=1, y=2 and z=3."
 },
 
 {
@@ -129,38 +129,6 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "reference.html#FixedPolynomials.evaluate",
-    "page": "Polynomial",
-    "title": "FixedPolynomials.evaluate",
-    "category": "Function",
-    "text": "evaluate(p::Polynomial{T}, x::AbstractVector{T})\n\nEvaluates p at x, i.e. p(x). Polynomial is also callable, i.e. you can also evaluate it via p(x).\n\n\n\nevaluate(g, x, cfg::GradientConfig)\n\nEvaluate g at x using the precomputated values in cfg. Note that this is usually signifcant faster than evaluate(g, x).\n\nExample\n\ncfg = GradientConfig(g)\nevaluate(g, x, cfg)\n\n\n\nevaluate(F, x, cfg::JacobianConfig)\n\nEvaluate the system F at x using the precomputated values in cfg. Note that this is usually signifcant faster than map(f -> evaluate(f, x), F).\n\nExample\n\ncfg = JacobianConfig(F)\nevaluate(F, x, cfg)\n\n\n\n"
-},
-
-{
-    "location": "reference.html#FixedPolynomials.weyldot",
-    "page": "Polynomial",
-    "title": "FixedPolynomials.weyldot",
-    "category": "Function",
-    "text": "weyldot(f::Polynomial, g::Polynomial)\n\nCompute the Bombieri-Weyl dot product. Note that this is only properly defined if f and g are homogenous.\n\nweyldot(f::Vector{Polynomial}, g::Vector{Polynomial})\n\nCompute the dot product for vectors of polynomials.\n\n\n\n"
-},
-
-{
-    "location": "reference.html#FixedPolynomials.weylnorm",
-    "page": "Polynomial",
-    "title": "FixedPolynomials.weylnorm",
-    "category": "Function",
-    "text": "weylnorm(f::Polynomial)\n\nCompute the Bombieri-Weyl norm. Note that this is only properly defined if f is homogenous.\n\n\n\n"
-},
-
-{
-    "location": "reference.html#Evaluation-1",
-    "page": "Polynomial",
-    "title": "Evaluation",
-    "category": "section",
-    "text": "evaluate\nweyldot\nweylnorm"
-},
-
-{
     "location": "reference.html#FixedPolynomials.differentiate",
     "page": "Polynomial",
     "title": "FixedPolynomials.differentiate",
@@ -201,11 +169,147 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "polynomialevaluationarray.html#",
-    "page": "PolynomialEvaluationArray",
-    "title": "PolynomialEvaluationArray",
+    "location": "reference.html#FixedPolynomials.weyldot",
+    "page": "Polynomial",
+    "title": "FixedPolynomials.weyldot",
+    "category": "Function",
+    "text": "weyldot(f::Polynomial, g::Polynomial)\n\nCompute the Bombieri-Weyl dot product. Note that this is only properly defined if f and g are homogenous.\n\nweyldot(f::Vector{Polynomial}, g::Vector{Polynomial})\n\nCompute the dot product for vectors of polynomials.\n\n\n\n"
+},
+
+{
+    "location": "reference.html#FixedPolynomials.weylnorm",
+    "page": "Polynomial",
+    "title": "FixedPolynomials.weylnorm",
+    "category": "Function",
+    "text": "weylnorm(f::Polynomial)\n\nCompute the Bombieri-Weyl norm. Note that this is only properly defined if f is homogenous.\n\n\n\n"
+},
+
+{
+    "location": "reference.html#Misc-1",
+    "page": "Polynomial",
+    "title": "Misc",
+    "category": "section",
+    "text": "weyldot\nweylnorm"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.GradientConfig",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.GradientConfig",
+    "category": "Type",
+    "text": "GradientConfig(f::Polynomial{T}, [x::AbstractVector{S}])\n\nA data structure with which the gradient of a Polynomial f can be evaluated efficiently. Note that x is only used to determine the output type of f(x).\n\nGradientConfig(f::Polynomial{T}, [S])\n\nInstead of a vector x a type can also be given directly.\n\n\n\n"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.JacobianConfig",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.JacobianConfig",
+    "category": "Type",
+    "text": "JacobianConfig(F::Vector{Polynomial{T}}, [x::AbstractVector{S}])\n\nA data structure with which the jacobian of a Vector F of Polynomials can be evaluated efficiently. Note that x is only used to determine the output type of F(x).\n\nJacobianConfig(F::Vector{Polynomial{T}}, [S])\n\nInstead of a vector x a type can also be given directly.\n\n\n\n"
+},
+
+{
+    "location": "performance.html#",
+    "page": "Fast Evaluation",
+    "title": "Fast Evaluation",
     "category": "page",
-    "text": "PolynomialEvaluationArray\nevaluate!\nprecompute!\nunsafe_evaluate"
+    "text": "In order to achieve a fast evaluation we need to precompute some things and also preallocate intermediate storage. For this we have GradientConfig and JacobianConfig:GradientConfig\nJacobianConfig"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.evaluate",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.evaluate",
+    "category": "Function",
+    "text": "evaluate(p::Polynomial{T}, x::AbstractVector{T})\n\nEvaluates p at x, i.e. p(x). Polynomial is also callable, i.e. you can also evaluate it via p(x).\n\n\n\nevaluate(g, x, cfg::GradientConfig)\n\nEvaluate g at x using the precomputated values in cfg. Note that this is usually signifcant faster than evaluate(g, x).\n\nExample\n\ncfg = GradientConfig(g)\nevaluate(g, x, cfg)\n\n\n\nevaluate(F, x, cfg::JacobianConfig)\n\nEvaluate the system F at x using the precomputated values in cfg. Note that this is usually signifcant faster than map(f -> evaluate(f, x), F).\n\nExample\n\ncfg = JacobianConfig(F)\nevaluate(F, x, cfg)\n\n\n\n"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.evaluate!",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.evaluate!",
+    "category": "Function",
+    "text": "evaluate!(u, F, x, cfg::JacobianConfig)\n\nEvaluate the system F at x using the precomputated values in cfg and store the result in u. Note that this is usually signifcant faster than map!(u, f -> evaluate(f, x), F).\n\nExample\n\ncfg = JacobianConfig(F)\nevaluate!(u, F, x, cfg)\n\n\n\n"
+},
+
+{
+    "location": "performance.html#Evaluation-1",
+    "page": "Fast Evaluation",
+    "title": "Evaluation",
+    "category": "section",
+    "text": "evaluate\nevaluate!"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.gradient",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.gradient",
+    "category": "Function",
+    "text": "gradient(g, x, cfg::GradientConfig)\n\nCompute the gradient of g at x using the precomputated values in cfg.\n\nExample\n\ncfg = GradientConfig(g)\ngradient(g, x, cfg)\n\n\n\ngradient(r::GradientDiffResult)\n\nGet the currently stored gradient in r.\n\n\n\n"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.gradient!",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.gradient!",
+    "category": "Function",
+    "text": "gradient!(u, g, x, cfg::GradientConfig)\n\nCompute the gradient of g at x using the precomputated values in cfg and store thre result in u.\n\nExample\n\ncfg = GradientConfig(g)\ngradient(u, g, x, cfg)\n\n\n\ngradient!(r::GradientDiffResult, g, x, cfg::GradientConfig)\n\nCompute g(x) and the gradient of g at x at once using the precomputated values in cfg and store thre result in r. This is faster than calling both values separetely.\n\nExample\n\ncfg = GradientConfig(g)\nr = GradientDiffResult(r)\ngradient!(r, g, x, cfg)\n\nvalue(r) == g(x)\ngradient(r) == gradient(g, x, cfg)\n\n\n\n"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.jacobian",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.jacobian",
+    "category": "Function",
+    "text": "jacobian!(u, F, x, cfg::JacobianConfig)\n\nEvaluate the jacobian of F at x using the precomputated values in cfg.\n\nExample\n\ncfg = JacobianConfig(F)\njacobian(F, x, cfg)\n\n\n\n"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.jacobian!",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.jacobian!",
+    "category": "Function",
+    "text": "jacobian!(u, F, x, cfg::JacobianConfig)\n\nEvaluate the jacobian of F at x using the precomputated values in cfg and store the result in u.\n\nExample\n\ncfg = JacobianConfig(F)\njacobian!(u, F, x, cfg)\n\n\n\njacobian!(r::JacobianDiffResult, F, x, cfg::JacobianConfig)\n\nCompute F(x) and the jacobian of F at x at once using the precomputated values in cfg and store thre result in r. This is faster than calling both values separetely.\n\nExample\n\ncfg = GradientConfig(g)\nr = GradientDiffResult(r)\ngradient!(r, g, x, cfg)\n\nvalue(r) == g(x)\ngradient(r) == gradient(g, x, cfg)\n\n\n\n"
+},
+
+{
+    "location": "performance.html#Derivatives-1",
+    "page": "Fast Evaluation",
+    "title": "Derivatives",
+    "category": "section",
+    "text": "FixedPolynomials.gradient\ngradient!\njacobian\njacobian!"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.GradientDiffResult",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.GradientDiffResult",
+    "category": "Type",
+    "text": "GradientDiffResult(cfg::GradientConfig)\n\nDuring the computation of g(x) we compute nearly everything we need for the evaluation of g(x). GradientDiffResult allocates memory to hold both values. This structure also signals gradient! to store g(x) and g(x).\n\nExample\n\ncfg = GradientConfig(g, x)\nr = GradientDiffResult(cfg)\ngradient!(r, g, x, cfg)\n\nvalue(r) == g(x)\ngradient(r) == gradient(g, x, cfg)\n\nGradientDiffResult(grad::AbstractVector)\n\nAllocate the memory to hold the gradient by yourself.\n\n\n\n"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.JacobianDiffResult",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.JacobianDiffResult",
+    "category": "Type",
+    "text": "JacobianDiffResult(cfg::GradientConfig)\n\nDuring the computation of the jacobian J_F(x) we compute nearly everything we need for the evaluation of F(x). JacobianDiffResult allocates memory to hold both values. This structure also signals jacobian! to store F(x) and J_F(x).\n\nExample\n\ncfg = JacobianConfig(F, x)\nr = JacobianDiffResult(cfg)\ngradient!(r, F, x, cfg)\n\nvalue(r) == map(f -> f(x), F)\njacobian(r) == jacobian(F, x, cfg)\n\nJacobianDiffResult(value::AbstractVector, jacobian::AbstractMatrix)\n\nAllocate the memory to hold the value and the jacobian by yourself.\n\n\n\n"
+},
+
+{
+    "location": "performance.html#FixedPolynomials.value",
+    "page": "Fast Evaluation",
+    "title": "FixedPolynomials.value",
+    "category": "Function",
+    "text": "value(r::GradientDiffResult)\n\nGet the currently stored value in r.\n\n\n\n"
+},
+
+{
+    "location": "performance.html#DiffResults-1",
+    "page": "Fast Evaluation",
+    "title": "DiffResults",
+    "category": "section",
+    "text": "GradientDiffResult\nJacobianDiffResult\nvalue"
 },
 
 ]}
